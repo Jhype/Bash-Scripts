eacc1a828f84a673edefd9d0272557ec90e6606f
Mateusz Piotrowski <mpp302@gmail.com>
2016-05-12 21:13:12 +0200

    Set absolute path for `cd` and simplify the way you provide your email.

--- sshkey.sh
+++ sshkey.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+EMAIL='' # Add your email here if you wish to use this script frequently.
+
+if [[ "$EMAIL" = "" && "$1" = "" ]]; then
+    echo "Usage: $0 <youremail@email.com>"
+    exit 1
+fi
+
+EMAIL="$1"
 sudo apt-get install xclip
-ssh-keygen -t rsa -b 4096 -C "youremail@email.com"
+ssh-keygen -t rsa -b 4096 -C "$EMAIL"
 ssh-add /root/.ssh/id_rsa
-cd .ssh
+cd ${HOME}/.ssh
 xclip -sel clip < id_rsa.pub
-git config --global user.email "youremail@email.com"
+git config --global user.email "$EMAIL"
 
